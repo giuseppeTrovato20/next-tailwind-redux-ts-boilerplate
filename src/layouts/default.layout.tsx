ad72d2b8fcc92dd7329d3830fa34a98478af0b66
Giuseppe Trovato <152182722+GiuseppeTrovatoKalpa@users.noreply.github.com>
2024-01-08 20:22:27 +0100

    boh

--- src/layouts/default.layout.tsx
+++ src/layouts/default.layout.tsx
@@ -2,7 +2,7 @@ import { BaseLayout } from "@/types/base-layout.type";
 
 export default function DefaultLayout({ children }: BaseLayout) {
   return (
-    <div data-theme="dark"  className="container mx-auto">
+    <div data-theme="dark" className="container mx-auto h-full">
       <main>{children}</main>
     </div>
   );
