e32b7ffb1b841b65dd5a42fcb3d0564a82f0077d
Giuseppe Trovato <giuseppe.trovato@kalpa.it>
2024-02-06 00:35:22 +0100

    retry pwa config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
+/** @type {import('next').NextConfig} */
 
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 const withPWA = require("next-pwa")({
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    register: true,
-    skipWaiting: true,
-  });
-  
-module.exports = withPWA({
-reactStrictMode: true,
-swcMinify: true,
-compiler: {
-    removeConsole: process.env.NODE_ENV !== "development",
-},
+  dest: "public",
+  register: true,
+  skipWaiting: true,
 });
 
+module.exports = withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "PWA treeecount",
-  description: "PWA treeecount to manage your count",
+  title: "PWA with Next 13",
+  description: "PWA application with Next 13",
   generator: "Next.js",
   manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
   themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
-  viewport: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  authors: [
+    { name: "Giuseppe Trovato" },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
   icons: [
     { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
     { rel: "icon", url: "icons/icon-128x128.png" },
   ],
-
 };
 
 export default function RootLayout({
