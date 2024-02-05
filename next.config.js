ddb1c37f3c748d77a18d1bc4608c4660e158ea9e
Giuseppe Trovato <giuseppe.trovato@kalpa.it>
2024-02-06 00:26:40 +0100

    changed next-config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        API: 'http://localhost:3001/'
-    },
-    output: 'export',
-    images: {
-        unoptimized: true
-    }
-}
+
 
 const withPWA = require("next-pwa")({
     dest: "public",
@@ -24,4 +15,3 @@ compiler: {
 },
 });
 
-module.exports = nextConfig
